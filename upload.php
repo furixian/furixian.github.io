09eb5f418188343f22372ce05d0bc672a8406dd2
furixian <122485227+furixian@users.noreply.github.com>
2023-10-25 22:00:22 +0300

    Create upload.php

---
+++ upload.php
@@ -0,0 +1,31 @@
+<?php
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$fileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+// Check if file already exists
+if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+}
+
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
+
+// Allow certain file formats
+if($fileType != "jpg" && $fileType != "png" && $fileType != "jpeg"
+&& $fileType != "gif" && $fileType != "txt" && $fileType != "pdf" && $fileType != "docx" && $fileType != "xlsx") {
+    echo "Sorry, only JPG, JPEG, PNG, GIF, TXT, PDF, DOCX, and XLSX files are allowed.";
+    $uploadOk = 0;
+}
+
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["fileToUpload"]
